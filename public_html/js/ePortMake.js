b866b9dffd61bfa548ba69725609d20b0156fbfb
xgao3 <xgao3@172.24.133.159>
2015-11-06 19:19:47 -0500

    trying to add tags and attributes

--- public_html/js/ePortMake.js
+++ public_html/js/ePortMake.js
@@ -11,14 +11,20 @@ function initePortfolio(){
 
 function loadData(jsonFile) {
     $.getJSON(jsonFile, function (json) {
+        alert(json.title);
         loadePortfolio(json);
         initPage();
     });
 }
 
 function loadePortfolio(ePortfolioData) {
-       title = ePortfolioData.title;
-
+    title = ePortfolioData.title;
+    for(var i =0;i<ePortfolioData.navbar.length;i++){
+        var e=document.createElement(type);
+        e.innerHTML = ePortfolioData.navbar[i].page;
+        navbar.appendChild(e);
+        e.attr("href",ePortfolioData.navbar[i].src);
+    }
 }
 
 function initPage(){
